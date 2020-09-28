e0688ae29d99e82b65e24dab7db6820e51ecb47b
Roman Letsuk <romaletsuk@gmail.com>
2020-09-28 18:55:40 +0300

    solved 2nd task

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,46 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+  ldr r2, [r0]
+  ldr r3, [r0, #16]
+  mul r2, r2, r3
+  ldr r3, [r0, #32]
+  mul r2, r2, r3
+  mov r1, r2
+
+  ldr r2, [r0, #4]
+  ldr r3, [r0, #20]
+  mul r2, r2, r3
+  ldr r3, [r0, #24]
+  mla r1, r2, r3, r1
+
+  ldr r2, [r0, #12]
+  ldr r3, [r0, #28]
+  mul r2, r2, r3
+  ldr r3, [r0, #8]
+  mla r1, r2, r3, r1
+
+  ldr r2, [r0, #16]
+  mul r2, r2, r3
+  ldr r3, [r0, #24]
+  mul r2, r2, r3
+  sub r1, r1, r2
+
+  ldr r2, [r0, #4]
+  ldr r3, [r0, #12]
+  mul r2, r2, r3
+  ldr r3, [r0, #32]
+  mul r2, r2, r3
+  sub r1, r1, r2
+
+  ldr r2, [r0]
+  ldr r3, [r0, #20]
+  mul r2, r2, r3
+  ldr r3, [r0, #28]
+  mul r2, r2, r3
+  sub r1, r1, r2
+
+  mov r0, r1
+
+  bx lr
