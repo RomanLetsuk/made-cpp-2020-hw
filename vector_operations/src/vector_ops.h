2975a103e10aed0023b71e01c0e66b202fb764b6
Roman Letsuk <romaletsuk@gmail.com>
2020-10-06 00:41:23 +0300

    solved 1st project

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -2,10 +2,20 @@
 #include <vector>
 #include <iostream>
 
+using std::vector;
 
 namespace task {
-
-// Your code here...
-
-
+  vector<double> operator +(vector<double> a, vector<double> b);
+  vector<double> operator -(vector<double> a, vector<double> b);
+  vector<double> operator +(vector<double> a);
+  vector<double> operator -(vector<double> a);
+  double operator *(vector<double> a, vector<double> b);
+  vector<double> operator %(vector<double> a, vector<double> b);
+  bool operator ||(vector<double> a, vector<double> b);
+  bool operator &&(vector<double> a, vector<double> b);
+  std::istream& operator >>(std::istream &in, vector<double> &v);
+  std::ostream& operator <<(std::ostream &os, vector<double> &v);
+  void reverse(vector<double> &a);
+  vector<int> operator |(vector<int> a, vector<int> b);
+  vector<int> operator &(vector<int> a, vector<int> b);
 }  // namespace task
